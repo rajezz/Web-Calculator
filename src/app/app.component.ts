6a6f1a96be83840f2d4e92a74bca03e5a9ea1088
Rajezz <35561689+rajezz@users.noreply.github.com>
2019-12-11 13:21:35 +0530

    preliminary
    first edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { get } from "http";
 
 @Component({
   selector: "app-root",
@@ -6,5 +7,42 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  
+  public static OperatorIs: string;
+  public static IsOperator: boolean;
+  public static firstOperand: number;
+  public static secondOperand: number;
+  public static Result: number;
+  public static expression: string;
+  public static setOperator(s: string): void {
+    this.OperatorIs = s;
+    this.IsOperator = true;
+    this.expression += s;
+  }
+  public static setNumber(a: number): void {
+    if (this.IsOperator) {
+      this.secondOperand = a;
+    } else {
+      this.firstOperand = a;
+    }
+    this.expression += a;
+  }
+  public static calculate(): void {
+    switch (this.OperatorIs) {
+      case "+":
+        this.Result = this.firstOperand + this.secondOperand;
+        break;
+      case "-":
+        this.Result = this.firstOperand - this.secondOperand;
+        break;
+      case "*":
+        this.Result = this.firstOperand * this.secondOperand;
+        break;
+      case "/":
+        this.Result = this.firstOperand / this.secondOperand;
+        break;
+      default:
+        this.Result = this.firstOperand;
+        break;
+    }
+  }
 }
